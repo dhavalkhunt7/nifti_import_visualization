464d109ce7923fc61feb0b7d98ffd78d6695cd37
Dhaval Khunt <dhavalkhunt07@gmail.com>
2022-09-28 11:01:05 +0200

    updating Task630_Patch_2d_Rat24h.py few things left to update

--- nnUNet_files/Task630_Patch_2d_Rat24h.py
+++ nnUNet_files/Task630_Patch_2d_Rat24h.py
@@ -39,44 +39,94 @@ def save_patches(patch_list_whole_img, output_folder, type, count):
     # if type is t2 or adc then output_folder is output_folder/imagesTs
     if count < 37:
         if type == "t2" or type == "adc":
-            output_folder = output_folder / "imagesTr"
+            output_folder = output_folder + "/imagesTr"
         elif type == "seg":
-            output_folder = output_folder / "labelsTr"
+            output_folder = output_folder + "/labelsTr"
     else:
         if type == "t2" or type == "adc":
-            output_folder = output_folder / "imagesTs"
+            output_folder = output_folder + "/imagesTs"
         elif type == "seg":
-            output_folder = output_folder / "labelsTs"
+            output_folder = output_folder + "/labelsTs"
 
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     for element in range(len(patch_list_whole_img)):
         patch = patch_list_whole_img[element]
         nii_file = nib.Nifti1Image(patch, np.eye(4))
-        nib.save(nii_file, output_folder + '/' + str(element) + '.nii.gz')
-        print('patch_' + str(element) + '.nii.gz saved')
+        if type == "t2":
+            new_name = new_dir_name + '_0001_' + str(element) + '.nii.gz'
+        elif type == "adc":
+            new_name = new_dir_name + '_0000_' + str(element) + '.nii.gz'
+        elif type == "seg":
+            new_name = new_dir_name + '_' + str(element) + '.nii.gz'
+        nib.save(nii_file, output_folder + '/' + new_name)
+    print(new_dir_name + ' saved')
 
 
 # %% create a function to create patches and save them
-# def create_and_save_patches(input_folder, output_folder, patch_size, overlap):
+def create_and_save_patches(input_file, output_folder, type_file, count):
+    load_nifti = nib.load(input_file)
+    input_image_array = np.array(load_nifti.dataobj)
 
+    patch_size = 31
+    overlap = 15
+    patch_list_whole_img = []
+    for k in range(input_image_array.shape[2]):
+        single_slice = input_image_array[:, :, k]
+        for i in range(0, single_slice.shape[0], patch_size - overlap):
+            for j in range(0, single_slice.shape[1], patch_size - overlap):
+                patch = single_slice[i:i + patch_size, j:j + patch_size]
+                if patch.shape == (patch_size, patch_size):
+                    patch_list_whole_img.append(patch)
+                # patch_list.append(patch)
+
+    list_patches = patch_list_whole_img
+
+    if count < 37:
+        if type_file == "t2" or type_file == "adc":
+            output_folder = output_folder + "/imagesTr"
+        elif type_file == "seg":
+            output_folder = output_folder + "/labelsTr"
+    else:
+        if type_file == "t2" or type_file == "adc":
+            output_folder = output_folder + "/imagesTs"
+        elif type_file == "seg":
+            output_folder = output_folder + "/labelsTs"
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    for element in range(len(patch_list_whole_img)):
+        patch = patch_list_whole_img[element]
+        nii_file = nib.Nifti1Image(patch, np.eye(4))
+        if type_file == "t2":
+            new_name = new_dir_name + '_0001_' + str(element) + '.nii.gz'
+        elif type_file == "adc":
+            new_name = new_dir_name + '_0000_' + str(element) + '.nii.gz'
+        elif type_file == "seg":
+            new_name = new_dir_name + '_' + str(element) + '.nii.gz'
+        nib.save(nii_file, output_folder + '/' + new_name)
+    print(new_dir_name + ' saved')
 
 
 # %%
 folder = input_folder / "Rat081-24h"
+output_fold = "patch_data/"
+
 for i in folder.glob("*.nii"):
     if "Masked_ADC" in i.name:
         adc_file = i
-        # print(adc_file)
+        create_and_save_patches(adc_file, output_fold, "adc", 1)
+
     elif "Masked_T2" in i.name:
         t2_file = i
-        # print(t2_file)
+        create_and_save_patches(t2_file, output_fold, "t2", 1)
+
     elif "GroundTruth24h" in i.name:
         gt_file = i
-        # print(gt_file)
-        img = nib.load(gt_file)
-        data = np.array(img.dataobj)
-
-        create_patches(data)
+        create_and_save_patches(gt_file, output_fold, "seg", 1)
 
 
+#%%
+output = Path("patch_data")
+for i in output.glob("*"):
+    print(i)
